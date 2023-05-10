105249d582dc6042b52e2fc155e7417602120d3e
Nathalia Pizzolato <111825084+nathpizzolato@users.noreply.github.com>
2023-05-10 11:08:36 -0300

    att

--- Aulas JavaScript/Exercicios-FOR/cres-des.js
+++ Aulas JavaScript/Exercicios-FOR/cres-des.js
@@ -1,3 +1,23 @@
 //Elaborar um programa que leia um número. Monte uma seta com * até o número informado
 //e após retornando a 1, conforme o exemplo.
 //Número: 8
+//< menor > maior
+
+const prompt = require("prompt-sync")();
+
+const numero = prompt("Numero: ");
+
+for(let i=1; i<numero; i++){
+    let seta = "";
+    for(let j=1; j<=i; j++){
+        seta = seta + "*"
+    }
+    console.log(`${seta}`);
+}
+for(let i=numero; i>=1; i--){
+    let seta = "";
+    for(let j=1; j<=i; j++){
+        seta = seta + "*"
+    }
+    console.log(`${seta}`)
+}
--- Aulas JavaScript/Exercicios-FOR/nome-asterisco.js
+++ Aulas JavaScript/Exercicios-FOR/nome-asterisco.js
@@ -1,12 +1,17 @@
 //Elaborar um programa que leia um número e um nome. Exiba o nome entre “*”. 
 //O número indica a quantidade de * que devem ser inseridas antes e após o nome.
 //Nome: Rosane Número: 10 ********** Rosane **********
+//< menor > maior
 
 const prompt = require("prompt-sync")()
 
 const nome = prompt("Nome: ");
 const numero = prompt("Numero de asteriscos: ");
+//console.log("*".repeat(numero) + " " + `${nome}` + " " + "*".repeat(numero));
+console.log();
 
-console.log("*".repeat(numero) + " " + `${nome}` + " " + "*".repeat(numero));
-
-
+let acumula = "";
+for(let i=1; i<=numero; i++){
+    acumula = acumula + "*";
+}
+console.log(`${acumula} ${nome} ${acumula}`);
