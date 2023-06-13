5e7749a8966b80016a92e023b8a097c46412723d
Nathalia Pizzolato <111825084+nathpizzolato@users.noreply.github.com>
2023-06-13 08:07:59 -0300

    att av

---
+++ Aulas JavaScript/Simulado-Atividade-Avaliativa2/primeiro.js
@@ -0,0 +1,24 @@
+const prompt = require("prompt-sync")()
+
+const numero = Number(prompt("Informe um Número: "));
+
+let soma = 0;
+let divisor = "";
+
+console.log("-".repeat(25));
+// i < numero porque se não ele pega o numero tbm
+for(let i=0; i<numero; i++){
+    if(numero%i == 0){
+        divisor = divisor + i + ", ";
+        soma = soma + i
+    }
+}
+ console.log(`Número: ${numero}`);
+ console.log(`Divisores: ${divisor}`);
+ console.log(`Soma dos Divisores: ${soma}`);
+
+ if(soma == numero){
+    console.log(`${numero} é um Número Perfeito!`);
+ }else{
+    console.log(`${numero} NÃO é um Número Perfeito!`);
+ }
---
+++ Aulas JavaScript/Simulado-Atividade-Avaliativa2/segundo.js
@@ -0,0 +1,8 @@
+const prompt = require("prompt-sync")();
+
+const numero = Number(prompt("Nº de Funcionários em 2023: "));
+let ano = 2023;
+
+for(i=0; i<=5; i++){
+    
+}
